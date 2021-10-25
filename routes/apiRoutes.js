ed759d30b37655413d5f77438f683750c71f9e00
Paul Barcenas <paulbarcenas@yahoo.com>
2021-10-24 20:45:30 -0500

    started post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,8 @@
-//const {notes} = require('../db/db.json');
+// const {notes} = require('../db/db.json');
 const router = require('express').Router();
 const fs = require('fs');
 const util = require('util');
-
+const notes = require('../db/db.json');
 const asyncRead = util.promisify(fs.readFile);
 
 
@@ -10,7 +10,7 @@ router.get('/notes', (req,res) => {
   // res.json(notes);
 
   asyncRead('db/db.json', 'utf8').then(data => {
-console.log("fdoasihdgifha",data);
+console.log(data);
 res.json(JSON.parse(data));
   }).catch(err=> {
    return res.json(err);
@@ -25,7 +25,21 @@ router.delete('/notes/:id', (req,res) => {
 })
 
 router.post('/notes', (req, res) => {
+let newNote = {
+  title: req.body.title,
+  text: req.body.text
+} ;
 
+
+notes.push(newNote);
+
+  fs.writeFile('db/db.json', notes, (err) => {
+    if (err) {
+      return res.json(err);
+    }
+
+    res.json(JSON.stringify(notes));
+  });
 });
 
 
